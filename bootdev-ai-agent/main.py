38e7ce2e1882f67e51365bd8772c023e6b41292a
tomaszklos <t.klos@protonmail.com>
2025-11-08 10:48:09 +0100

    system prompt added to the prompt

--- bootdev-ai-agent/main.py
+++ bootdev-ai-agent/main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import system_prompt
 
 
 def main():
@@ -31,6 +32,7 @@ def main():
         response = client.models.generate_content(
             model="gemini-2.0-flash-001",
             contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt)
         )
         if verbose:   
             print(f"User prompt: {user_prompt}")
