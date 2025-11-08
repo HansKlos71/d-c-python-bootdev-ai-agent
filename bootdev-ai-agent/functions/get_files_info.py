5e030bdfb89903b9937cabb45ed5b054ebc4dc54
tomaszklos <t.klos@protonmail.com>
2025-11-08 09:40:08 +0100

    Added testing for get_files_content function

--- bootdev-ai-agent/functions/get_files_info.py
+++ bootdev-ai-agent/functions/get_files_info.py
@@ -1,15 +1,40 @@
 import os
 
+
 def get_files_info(working_directory, directory="."):
     # directory param is a relative path whitin the working_directory
+    
     absolute_working_directory = os.path.abspath(working_directory)
     absolute_path = os.path.abspath(os.path.join(working_directory, directory))
+
     if not absolute_path.startswith(absolute_working_directory):
         return f"Error: Cannot list {directory} as it is outside the working directory."
     if not os.path.isdir(absolute_path):
         return f"Error: {directory} is not a directory."
+    
     files = os.listdir(absolute_path)
     file_info_list = ""
+    
     for file in files:
         file_info_list += f"- {file}: file_size={os.path.getsize(os.path.join(absolute_path, file))} bytes, is_dir={os.path.isdir(os.path.join(absolute_path, file))}\n"
+    
     return file_info_list
+
+
+def get_file_content(working_directory, file_path):
+
+    absolute_working_directory = os.path.abspath(working_directory)
+    absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not absolute_path.startswith(absolute_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(absolute_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    MAX_CHARS = 10000
+
+    with open(absolute_path, "r") as f:
+        content = f.read()
+        if len(content) > MAX_CHARS:
+            content = content[:MAX_CHARS] + f"[...File {file_path} truncated at 10000 characters]"
+    return content
