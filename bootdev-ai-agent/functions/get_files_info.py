1240d42db29b19b92451bce96f4dae0a9540944a
tomaszklos <t.klos@protonmail.com>
2025-11-08 10:20:41 +0100

    Added functionn run python file

--- bootdev-ai-agent/functions/get_files_info.py
+++ bootdev-ai-agent/functions/get_files_info.py
@@ -57,3 +57,30 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: Failed to write to {file_path}: {str(e)}"
+    
+
+def run_python_file(working_directory, file_path, args=[]):
+    absolute_working_directory = os.path.abspath(working_directory)
+    absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not absolute_path.startswith(absolute_working_directory):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(absolute_path):
+        return f'Error: File "{file_path}" not found.'
+    if not absolute_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    import subprocess
+    
+    try:
+        complete_process = subprocess.run(["python3", absolute_path] + args, capture_output=True, timeout=30)
+        print(f"STDOUT:\n{complete_process.stdout.decode()}")
+        print(f"STDERR:\n{complete_process.stderr.decode()}")
+
+        if complete_process.returncode != 0:
+            print(f"Error: PRocess exited with code X")
+        if complete_process.stdout.strip() == None:
+            print("No output produced.")
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
--- bootdev-ai-agent/tests.py
+++ bootdev-ai-agent/tests.py
@@ -1,6 +1,15 @@
 import os
-from functions.get_files_info import write_file
+from functions.get_files_info import run_python_file
 
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+print(run_python_file("calculator", "main.py")) # (should print the calculator's usage instructions)
+print("----------------------------------------")
+print(run_python_file("calculator", "main.py", ["3 + 5"])) # (should run the calculator... which gives a kinda nasty rendered result)
+print("----------------------------------------")
+print(run_python_file("calculator", "tests.py"))
+print("----------------------------------------")
+print(run_python_file("calculator", "../main.py")) # (this should return an error)
+print("----------------------------------------")
+print(run_python_file("calculator", "nonexistent.py")) # (this should return an error)
+print("----------------------------------------")
+print(run_python_file("calculator", "lorem.txt")) # (this should return an error)
