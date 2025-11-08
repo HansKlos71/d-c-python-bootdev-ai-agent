c52e9e516465fb00aeb98cdb1207979fbabcf90d
tomaszklos <t.klos@protonmail.com>
2025-11-08 09:51:36 +0100

    Added function to overwrite/create a file

--- bootdev-ai-agent/functions/get_files_info.py
+++ bootdev-ai-agent/functions/get_files_info.py
@@ -38,3 +38,22 @@ def get_file_content(working_directory, file_path):
         if len(content) > MAX_CHARS:
             content = content[:MAX_CHARS] + f"[...File {file_path} truncated at 10000 characters]"
     return content
+
+
+def write_file(working_directory, file_path, content):
+    absolute_working_directory = os.path.abspath(working_directory)
+    absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not absolute_path.startswith(absolute_working_directory):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(absolute_working_directory):
+        try:
+            os.makedirs(absolute_working_directory)
+        except Exception as e:
+            return f"Error: Failed to create directories for {file_path}: {str(e)}"
+    try:
+        with open(absolute_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: Failed to write to {file_path}: {str(e)}"
